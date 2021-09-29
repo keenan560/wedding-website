65696642a2c8eac8c503a46d30554850c46e6324
keenan560 <keenan560@gmail.com>
2021-09-28 19:22:28 -0500

    added photos, now need to finish our story

---
+++ wedding-website/src/assets/photos_page/gallery.js
@@ -0,0 +1,86 @@
+import one from "./one.jpg";
+import two from "./two.jpg";
+import three from "./three.jpg";
+import four from "./four.jpg";
+import five from "./five.jpg";
+import six from "./six.jpg";
+import seven from "./seven.jpg";
+import eight from "./eight.jpg";
+import nine from "./nine.jpg";
+import ten from "./ten.jpg";
+import eleven from "./eleven.jpg";
+import twelve from "./twelve.jpg";
+import thirteen from "./thirteen.jpg";
+import fourteen from "./fourteen.jpg";
+import fifteen from "./fifteen.jpg";
+import sixteen from "./sixteen.jpg";
+import seventeen from "./seventeen.jpg";
+import eighteen from "./eighteen.jpg";
+import nineteen from "./nineteen.jpg";
+import twenty from "./twenty.jpg";
+
+const photos = [
+  {
+    url: one,
+  },
+  {
+    url: two,
+  },
+  {
+    url: three,
+  },
+  {
+    url: four,
+  },
+  {
+    url: five,
+  },
+  {
+    url: six,
+  },
+  {
+    url: seven,
+  },
+  {
+    url: eight,
+  },
+  {
+    url: nine,
+  },
+  {
+    url: ten,
+  },
+  {
+    url: eleven,
+  },
+  {
+    url: twelve,
+  },
+  {
+    url: thirteen,
+  },
+  {
+    url: fourteen,
+  },
+  {
+    url: fifteen,
+  },
+  {
+    url: sixteen,
+  },
+  {
+    url: seventeen,
+  },
+
+  {
+    url: eighteen,
+  },
+  {
+    url: nineteen,
+  },
+  {
+    url: twenty,
+  },
+];
+
+export default photos;
--- wedding-website/src/components/Photos.js
+++ wedding-website/src/components/Photos.js
@@ -1,10 +1,76 @@
 import React from "react";
 import "./Photos.css";
+import photos from "../assets/photos_page/gallery";
 
 function Photos() {
   return (
-    <div className="photos__container">
-      <h1>Photos</h1>
+    <div>
+      <h1 className="photo__title ">Gallery</h1>
+      <div className="photos__container mx-4">
+        <div
+          id="carouselExampleControls"
+          className="carousel slide"
+          data-bs-ride="carousel"
+        >
+          <div className="carousel-inner">
+            {/* <div class="carousel-item active">
+            <img src="..." class="d-block w-100" alt="..." />
+          </div>
+          <div class="carousel-item">
+            <img src="..." class="d-block w-100" alt="..." />
+          </div>
+          <div class="carousel-item">
+            <img src="..." class="d-block w-100" alt="..." />
+          </div> */}
+            {photos.map((photo, idx) =>
+              idx === 0 ? (
+                <div className="carousel-item active animate__animated animate__fadeIn animate">
+                  <img
+                    key={idx}
+                    src={photo.url}
+                    className="img-thumbnail-gal animate__animated animate__fadeIn animate"
+                    alt={idx}
+                  />
+                </div>
+              ) : (
+                <div className="carousel-item animate__animated animate__fadeIn animate ">
+                  <img
+                    key={idx}
+                    src={photo.url}
+                    className="img-thumbnail-gal animate__animated animate__fadeIn animate"
+                    alt={idx}
+                  />
+                </div>
+              )
+            )}
+          </div>
+          <button
+            className="carousel-control-prev"
+            type="button"
+            data-bs-target="#carouselExampleControls"
+            data-bs-slide="prev"
+          >
+            <span
+              className="carousel-control-prev-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button
+            className="carousel-control-next"
+            type="button"
+            data-bs-target="#carouselExampleControls"
+            data-bs-slide="next"
+          >
+            <span
+              className="carousel-control-next-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Next</span>
+          </button>
+        </div>
+        <h1 className="quote">All the good memories...</h1>
+      </div>
     </div>
   );
 }
